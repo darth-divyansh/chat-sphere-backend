f2503d616c1179b5449937e53901c6071c9b5dc1
Divyansh Bansal <bansal9divyansh@gmail.com>
2025-07-11 16:36:26 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    // allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
