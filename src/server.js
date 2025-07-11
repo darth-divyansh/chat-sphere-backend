68a630537f6cbdc12c8024f35df2afbbb0e9bebc
Divyansh Bansal <bansal9divyansh@gmail.com>
2025-07-11 14:03:00 +0530

    Update frontend url

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5001; // Change to 5001 to match frontend
 const __dirname = path.resolve();
 app.use(
   cors({
-    origin: "https://preeminent-narwhal-001378.netlify.app/",
+    origin: "https://preeminent-narwhal-001378.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
